c6977b8660d9d4abbbb2e602467d5a02776e7211
Karthiksuriya01 <karthiksuriy01@gmail.com>
2024-06-23 02:15:49 +0530

    Updated

---
+++ Anime_Website/script.js
@@ -0,0 +1,186 @@
+let sliders = 
+[
+        {
+            "name": "The Irregular at Magic High School Season 3",
+            "jname": "Mahouka Koukou no Rettousei 3rd Season",
+            "spotlight": "#1 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/bf/89/bf89b0a996b322db0fe08d2245d7b645/bf89b0a996b322db0fe08d2245d7b645.jpg",
+            "format": "TV",
+            "duration": "23m",
+            "release": "Apr 5, 2024",
+            "quality": "HD",
+            "animeId": "the-irregular-at-magic-high-school-season-3-19142",
+            "anidesc": "Miyuki turns out to be a magical prodigy. However, Tatsuya lacks magical prowess and has to rely on his smarts and technical know-how instead. This makes him The Irregular At Magic High School."
+        },
+        {
+            "name": "One Piece",
+            "jname": "One Piece",
+            "spotlight": "#2 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/db/86/db8603d2f4fa78e1c42f6cf829030a18/db8603d2f4fa78e1c42f6cf829030a18.jpg",
+            "format": "TV",
+            "duration": "24m",
+            "release": "Oct 20, 1999",
+            "quality": "HD",
+            "animeId": "one-piece-100",
+            "anidesc": "Gold Roger was known as the \"Pirate King,\" the strongest and most infamous being to have sailed the Grand Line. The capture and execution of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King.\n\nEnter Monkey Luffy, a 17-year-old boy who defies your standard definition of a pirate. Rather than the popular persona of a wicked, hardened, toothless pirate ransacking villages for fun, Luffy's reason for being a pirate is one of pure wonder: the thought of an exciting adventure that leads him to intriguing people and ultimately, the promised treasure. Following in the footsteps of his childhood hero, Luffy and his crew travel across the Grand Line, experiencing crazy adventures, unveiling dark mysteries and battling strong enemies, all in order to reach the most coveted of all fortunes—One Piece.\n\n[Written by MAL Rewrite]"
+        },
+        {
+            "name": "A Condition Called Love",
+            "jname": "Hananoi-kun to Koi no Yamai",
+            "spotlight": "#3 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/51/bc/51bc97e50656871807b5da3399191f63/51bc97e50656871807b5da3399191f63.jpg",
+            "format": "TV",
+            "duration": "23m",
+            "release": "Apr 4, 2024",
+            "quality": "HD",
+            "animeId": "a-condition-called-love-19138",
+            "anidesc": "Hotaru Hinase has lived nearly sixteen years without romance. She is satisfied alone as long as she can enjoy the little things in life with her beloved friends and family. In her mind, she is not meant to fall in love, nor does she understand the concept.\n\nWhile out with a friend, Hotaru witnesses the severe breakup of her schoolmate Hananoi, to whom she has never spoken. Afterward, she sees him sitting in the snow and holds her umbrella over him. To her surprise, this gesture results in him confessing his feelings for her at school the next day. Though Hotaru rejects him, Hananoi insists on letting her get to know him. He does whatever he can to woo her, from changing his hairstyle to finding her lost hairpin in the snow.\n\nAlthough she has no romantic feelings towards him, Hotaru believes that by spending more time with him, she will learn how to love. She agrees to date him and slowly begins to navigate what being a girlfriend entails, including how to reciprocate the kind gestures that Hananoi continues to perform for her."
+        },
+        {
+            "name": "Wind Breaker",
+            "jname": "Wind Breaker",
+            "spotlight": "#4 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/b8/b1/b8b1bbc386d81a95c40e236089e11312/b8b1bbc386d81a95c40e236089e11312.jpg",
+            "format": "TV",
+            "duration": "23m",
+            "release": "Apr 5, 2024",
+            "quality": "HD",
+            "animeId": "wind-breaker-19136",
+            "anidesc": "Haruka Sakura wants nothing to do with weaklings—he's only interested in the strongest of the strong. He's just started at Furin High School, a school of degenerates known only for their brawling strength—strength they use to protect their town from anyone who wishes it ill. But Haruka's not interested in being a hero or being part of any sort of team—he just wants to fight his way to the top!"
+        },
+        {
+            "name": "I Was Reincarnated as the 7th Prince so I Can Take My Time Perfecting My Magical Ability",
+            "jname": "Tensei shitara Dainana Ouji Datta node, Kimama ni Majutsu wo Kiwamemasu",
+            "spotlight": "#5 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/d7/c8/d7c84728f79078a9c47fa861a3a8e627/d7c84728f79078a9c47fa861a3a8e627.jpg",
+            "format": "TV",
+            "duration": "23m",
+            "release": "Apr 2, 2024",
+            "quality": "HD",
+            "animeId": "i-was-reincarnated-as-the-7th-prince-so-i-can-take-my-time-perfecting-my-magical-ability-19132",
+            "anidesc": "In his past life, Prince Lloyd de Saloum was a commoner who could not become adept at magic, no matter how knowledgeable or obsessed he was about it. Now reincarnated into his current royal lineage, he receives a body with seemingly endless mana, making his desire to master all things arcane attainable. Moreover, as the seventh prince of the kingdom, Lloyd has no claim to the throne, allowing him to nurture his abilities as freely as he wants.\n\nUnfortunately for Lloyd, ancient seals that imprison powerful demons begin to break down and release the horrors within, endangering the peace. With these monstrosities roaming around, Lloyd's overwhelming magical prowess is the ultimate weapon that can neutralize these threats before all things descend to chaos."
+        },
+        {
+            "name": "The Duke of Death and His Maid Season 3",
+            "jname": "Shinigami Bocchan to Kuro Maid 3rd Season",
+            "spotlight": "#6 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/27/75/27756cc6545d8c73d47e31dd2315910e/27756cc6545d8c73d47e31dd2315910e.jpg",
+            "format": "TV",
+            "duration": "23m",
+            "release": "Apr 7, 2024",
+            "quality": "HD",
+            "animeId": "the-duke-of-death-and-his-maid-season-3-19128",
+            "anidesc": "The third season of Shinigami Bocchan to Kuro Maid.\n\nThe Duke and Alice will have their fates collide as their untouchable relationship comes to its conclusion!"
+        },
+        {
+            "name": "Demon Slayer: Kimetsu no Yaiba Hashira Training Arc",
+            "jname": "Kimetsu no Yaiba: Hashira Geiko-hen",
+            "spotlight": "#7 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/46/d8/46d8e6d3fcd4a016ff5e90f0281eae76/46d8e6d3fcd4a016ff5e90f0281eae76.jpg",
+            "format": "TV",
+            "duration": "24m",
+            "release": "May 12, 2024",
+            "quality": "HD",
+            "animeId": "demon-slayer-kimetsu-no-yaiba-hashira-training-arc-19107",
+            "anidesc": "Adaptation of the Hashira Training Arc.\n\nThe Hashira, the Demon Slayer Corps' highest ranking swordsmen and members. The Hashira Training has begun in order to face the forthcoming battle against Muzan Kibutsuji. Each with their own thoughts and hopes held in their hearts, a new story for Tanjiro and the Hashira begins."
+        },
+        {
+            "name": "The New Gate",
+            "jname": "The New Gate",
+            "spotlight": "#8 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/6e/24/6e2400c551da2d8a07be697773bdbf0d/6e2400c551da2d8a07be697773bdbf0d.jpg",
+            "format": "TV",
+            "duration": "22m",
+            "release": "Apr 14, 2024",
+            "quality": "HD",
+            "animeId": "the-new-gate-19122",
+            "anidesc": "\"The New Gate\" is an online life-or-death game with tens of thousands of players. Thanks to Shin, the most skilled veteran player, the other players will finally be released from the game. Shin has killed the last boss and believes he is finally able to escape when he is blinded by a flash of mysterious light. He awakes to find himself in the game's world 500 years later! So begins a new chapter in the life of an unsurpassed legendary player!"
+        },
+        {
+            "name": "Delicious in Dungeon",
+            "jname": "Dungeon Meshi",
+            "spotlight": "#9 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/50/af/50affe2ea9a02c36d5a7c0532c1b7ef9/50affe2ea9a02c36d5a7c0532c1b7ef9.jpeg",
+            "format": "TV",
+            "duration": "25m",
+            "release": "Jan 4, 2024",
+            "quality": "HD",
+            "animeId": "delicious-in-dungeon-18506",
+            "anidesc": "Adventuring knight Laios Touden leads a small party through a seemingly endless dungeon, a subterranean maze full of dangerous monsters and precarious traps. Through the use of advanced magic, an explorer can sometimes be resurrected, allowing them to learn from past mistakes and give traversing the dungeon another go. However, when a powerful dragon eats Falin, Laios' spellcasting sister, she sends her brother and his companions back to the beginning to save them from permanent ends.\n\nThough strapped for cash and equipment, Laios resolves to fight his way through the dungeon and rescue Falin before she can be digested by the dragon. Despite some of Laios' allies abandoning him, two remain by his side: elven mage Marcille Donato and halfling locksmith Chilchuck Tims. Due to their lack of funds, the party faces the daunting prospect of starving before being able to complete their quest. However, they find salvation in Senshi, a dwarven warrior with a penchant for cooking and safely eating defeated monsters.\n\nWith Senshi's culinary expertise, Laios and his companions struggle through the dungeon while learning about gourmet dining—and each other—along the way."
+        },
+        {
+            "name": "Ninja Kamui",
+            "jname": "Ninja Kamui",
+            "spotlight": "#10 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/9c/58/9c582bdfc9a2425c58b36a044714d5b5/9c582bdfc9a2425c58b36a044714d5b5.jpg",
+            "format": "TV",
+            "duration": "22m",
+            "release": "Feb 11, 2024",
+            "quality": "HD",
+            "animeId": "ninja-kamui-19018",
+            "anidesc": "Joe Higan is a Nukenin – a former ninja who escaped his clan and is hiding from his violent past in rural America with his family. One night, he is ambushed by a team of assassins from his former organization who exact a bloody retribution on Joe and his family for betraying their ancient code. Rising from his seeming \"death,\" Joe will re-emerge as his former self – Ninja Kamui – to avenge his family and friends. Kamui is a 21st century ninja, a shadowy anachronism who pits his ancient skills against high-tech weaponry with brutal finesse. He must face off against trained assassins, combat cyborgs, and rival ninjas to bring down the very clan that made him."
+        },
+        {
+            "name": "The Apothecary Diaries",
+            "jname": "Kusuriya no Hitorigoto",
+            "spotlight": "#11 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/31/aa/31aacbf403a8f1c180a940009dbf17f5/31aacbf403a8f1c180a940009dbf17f5.jpeg",
+            "format": "TV",
+            "duration": "24m",
+            "release": "Oct 22, 2023",
+            "quality": "HD",
+            "animeId": "the-apothecary-diaries-18578",
+            "anidesc": "Maomao, an apothecary's daughter, has been plucked from her peaceful life and sold to the lowest echelons of the imperial court. Now merely a maid, Maomao settles into her new mundane life and hides her extensive knowledge of medicine in order to avoid any unwanted attention.\n\nNot long after Maomao's arrival, the emperor's infant children inexplicably begin to experience grave symptoms—almost as if a curse has been cast. The curious Maomao easily solves the mystery and, to remain out of the limelight, attempts to leave an anonymous tip. Unfortunately, the dashing and perceptive eunuch Jinshi sees through it and manages to single her out.\n\nIn recognition of her talent, Maomao is promoted to lady-in-waiting for the emperor's favorite concubine, Gyokuyou. As Maomao continues to remedy the numerous ailments afflicting the imperial court, her pharmaceutical expertise quickly proves indispensable."
+        },
+        {
+            "name": "Bleach",
+            "jname": "Bleach",
+            "spotlight": "#12 Spotlight",
+            "imageAnime": "https://img.flawlessfiles.com/_r/1366x768/100/58/d0/58d0b99666b285d2c484fec5dfaa23f0/58d0b99666b285d2c484fec5dfaa23f0.jpg",
+            "format": "TV",
+            "duration": "24m",
+            "release": "Oct 5, 2004",
+            "quality": "HD",
+            "animeId": "bleach-806",
+            "anidesc": "Ichigo Kurosaki is an ordinary high schooler—until his family is attacked by a Hollow, a corrupt spirit that seeks to devour human souls. It is then that he meets a Soul Reaper named Rukia Kuchiki, who gets injured while protecting Ichigo's family from the assailant. To save his family, Ichigo accepts Rukia's offer of taking her powers and becomes a Soul Reaper as a result.\n\nHowever, as Rukia is unable to regain her powers, Ichigo is given the daunting task of hunting down the Hollows that plague their town. However, he is not alone in his fight, as he is later joined by his friends—classmates Orihime Inoue, Yasutora Sado, and Uryuu Ishida—who each have their own unique abilities. As Ichigo and his comrades get used to their new duties and support each other on and off the battlefield, the young Soul Reaper soon learns that the Hollows are not the only real threat to the human world.\n\n[Written by MAL Rewrite]"
+        }
+]
+
+// import {animeList} from "./main.js"
+
+let currentIndex = 0;
+const main = document.querySelector('main');
+let prevButton = document.getElementById('prevBtn');
+let nextButton = document.getElementById('nextBtn');
+
+function initSlider() {
+    const currentSlider = sliders[currentIndex];
+    main.style.backgroundImage = `url(${currentSlider.imageAnime})`
+    main.style.backgroundRepeat = "no-repeat"
+    main.style.backgroundSize = "cover"
+    main.style.backgroundPosition = "center"
+}
+console.log(sliders[currentIndex])
+function prevSlide() 
+{
+    currentIndex = (currentIndex === 0) ? sliders.length - 1 : currentIndex - 1;
+    initSlider();
+    
+}
+
+function nextSlide() 
+{
+        
+    currentIndex = (currentIndex === sliders.length - 1) ? 0 : currentIndex + 1;
+      
+    initSlider(); 
+    
+}
+
+
+prevButton.addEventListener("click", prevSlide)
+nextButton.addEventListener("click", nextSlide)
+
+setInterval(nextSlide, 5000);
+
+initSlider()
